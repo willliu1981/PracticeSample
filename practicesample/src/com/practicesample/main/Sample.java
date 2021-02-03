682979cc601a7549f0ba8e62384527ff468e4462
KuanWei <KuanWei@DESKTOP-8R38C1M>
2021-02-04 04:17:18 +0800

    init

---
+++ practicesample/src/com/practicesample/main/Sample.java
@@ -0,0 +1,15 @@
+package com.practicesample.main;
+
+
+public class Sample {
+	public static void main(String[] args) throws Exception {
+		IPractice.run("src\\com.practicesample.main");
+	}
+}
+
+class P {
+	public static void main(String[] args) throws Exception {
+
+	}
+}
+
